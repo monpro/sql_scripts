b6f528a73dc24c67d8af15ca70fbc02caec854bf
monpro <monproshen@gmail.com>
2021-03-04 22:28:01 +1100

    add sql script SalesAnalysis using with clause and group by operation to select the id with max_sum_price

---
+++ scripts/SalesAnalysisI.sql
@@ -0,0 +1,45 @@
+/**
+Write an SQL query that reports the best seller by total sales price, If there is a tie, report them all.
+
+The query result format is in the following example:
+
+Product table:
++------------+--------------+------------+
+| product_id | product_name | unit_price |
++------------+--------------+------------+
+| 1          | S8           | 1000       |
+| 2          | G4           | 800        |
+| 3          | iPhone       | 1400       |
++------------+--------------+------------+
+
+Sales table:
++-----------+------------+----------+------------+----------+-------+
+| seller_id | product_id | buyer_id | sale_date  | quantity | price |
++-----------+------------+----------+------------+----------+-------+
+| 1         | 1          | 1        | 2019-01-21 | 2        | 2000  |
+| 1         | 2          | 2        | 2019-02-17 | 1        | 800   |
+| 2         | 2          | 3        | 2019-06-02 | 1        | 800   |
+| 3         | 3          | 4        | 2019-05-13 | 2        | 2800  |
++-----------+------------+----------+------------+----------+-------+
+
+Result table:
++-------------+
+| seller_id   |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Both sellers with id 1 and 3 sold products with the most total price of 2800.
+
+Solution: use with clause to cache the max_sum_price. The use group by operation to select the seller_id.
+ */
+with max_price_table as (
+  select sum(price) as max_price from Sales
+  group by seller_id
+  order by max_price desc
+  limit 1
+)
+
+select seller_id from sales
+group by seller_id
+having sum(price) = (select max_price from max_price_table);
